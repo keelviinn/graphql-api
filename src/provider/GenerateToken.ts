8f77edde13ac2a2ca713a26c58be401e506b9c27
Kelvin Oliveira <keelviinn@gmail.com>
2021-08-12 19:14:34 -0300

    try - catch

--- src/provider/GenerateToken.ts
+++ src/provider/GenerateToken.ts
@@ -1,12 +1,14 @@
-import { AuthenticationError } from 'apollo-server-express';
-import { add } from 'date-fns';
-import { sign, verify } from 'jsonwebtoken';
+import { sign } from 'jsonwebtoken';
 
 const appSecret: any = process.env.APP_SECRET
 
 class GenerateToken {
-  generate(userDetails: any, userId: string): string {
-    return sign({...userDetails }, appSecret, { subject: userId.toString(), expiresIn: 60 * 15 });
+  generate(userDetails: any, userId: string): string | any {
+    try {
+      return sign({...userDetails }, appSecret, { subject: userId.toString(), expiresIn: 60 * 15 });      
+    } catch (error) {
+      console.error(error)
+    }
   }
 }
 
