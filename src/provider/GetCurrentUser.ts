f1f94350c6096043544ec42e88cc58f0508a4bff
Kelvin Oliveira <keelviinn@gmail.com>
2021-09-20 21:08:24 -0300

    fix getCurrentUser

--- src/provider/GetCurrentUser.ts
+++ src/provider/GetCurrentUser.ts
@@ -1,7 +1,7 @@
 import UserSchema from '../models/user/user.schema';
 import UserModel from '../models/user/user.model';
 
-export async function getCurrentUser(id: string): Promise<null | UserModel> {
+export async function getCurrentUser(id: string): Promise<UserModel | null> {
   if (!id) return null;
   return await UserSchema.findById(id).select('-password');
 }
