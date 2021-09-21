7ac5ae01153e49fdef96b11e23f9bf3631da2646
Kelvin Oliveira <keelviinn@gmail.com>
2021-09-20 21:14:39 -0300

    teste

--- src/provider/generateTokens.ts
+++ src/provider/generateTokens.ts
@@ -2,11 +2,6 @@ import { sign } from "jsonwebtoken";
 import { add } from 'date-fns';
 import RefreshToken from '../models/refreshToken/refreshToken.schema';
 
-interface ITokens {
-  refreshToken: string;
-  accessToken: string;
-}
-
 export const generateAccessToken = ({ userId }): string => {
   const accessToken = sign({ id: userId }, process.env.APP_SECRET as string, { expiresIn: 60 * 60 * 12 });
   return accessToken;
